5ef1c88a5c84422e116bb297ff5269e60be995a8
lijianpeng <1060397920@qq.com>
2018-02-27 16:20:43 +0800

    环境测试

---
+++ src/component/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{val}}
+    </div>
+</template>
+
+<script>
+export default {
+          data() {
+            return {
+                val: 'vue脚手架测试'
+            }
+        },
+}
+</script>
+
+<style scoped>
+
+</style>
