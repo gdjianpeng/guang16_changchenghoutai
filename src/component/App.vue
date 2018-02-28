698f5917b4b2ef2591d51276ec5d2fbac5d53a33
lijianpeng <1060397920@qq.com>
2018-02-28 10:16:09 +0800

    集成插件

--- src/component/App.vue
+++ src/component/App.vue
@@ -1,19 +1,31 @@
+
 <template>
-    <div>
-        {{val}}
-        <router-view></router-view>
-    </div>
+  <div>
+    <p>{{val}}</p>
+    <router-view></router-view>
+    <el-switch v-model="fufei" active-text="按月付费" inactive-text="按年付费">
+    </el-switch>
+
+  </div>
 </template>
+
+
 
 <script>
 export default {
-          data() {
-            return {
-                val: 'vue脚手架测试'
-            }
-        },
-}
+  data() {
+    return {
+      val: "vue脚手架测试",
+      fufei: true
+    };
+  },
+  // 生命周期函数, vue会自动调用
+  created() {
+    this.$http.get(this.$api.logout).then(() => console.log("退出登陆"));
+  }
+};
 </script>
+
 
 <style scoped>
 
--- src/component/login/Login.vue
+++ src/component/login/Login.vue
@@ -1,14 +1,37 @@
 <template>
     <div>
-        登录
+        <button @click="login">登陆</button>
+        <button @click="isLogin">登陆检测</button>
     </div>
 </template>
 
 <script>
     export default {
-        
+        data() {
+            return {
+                user: {
+                    uname: "admin",
+                    upwd: "123456"
+                }
+            };
+        },
+
+        methods: {
+            // 登陆
+            login() {
+                this.$http.post(this.$api.login, this.user)
+                    .then(rsp => alert(rsp.data.message.realname));
+            },
+
+            // 判断是否已登陆
+            isLogin() {
+                this.$http.get(this.$api.islogin)
+                    .then(rsp => alert(rsp.data.code));
+            }
+        }
     }
 </script>
+
 
 <style scoped>
 
