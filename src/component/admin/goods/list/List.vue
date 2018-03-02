f2d53bfc768771a85472b9eb2de8707a86f756aa
lijianpeng <1060397920@qq.com>
2018-03-02 16:36:40 +0800

    面包屑导航与按钮组

--- src/component/admin/goods/list/List.vue
+++ src/component/admin/goods/list/List.vue
@@ -1,15 +1,35 @@
 <template>
     <div>
-        商品列表
+        <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+            <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+        </el-breadcrumb>
+
+        <section class="list_btns">
+            <el-button plain size="mini" icon="el-icon-plus">新增</el-button>
+            <el-button plain size="mini" icon="el-icon-check">全选</el-button>
+            <el-button plain size="mini" icon="el-icon-delete">删除</el-button>
+
+            <div class="list_btns_right">
+                <el-input placeholder="请输入内容" prefix-icon="el-icon-search" label-width="200px" size="mini">
+                </el-input>
+            </div>
+        </section>
+
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+    .list_btns {
+    margin-top: 30px;
+    &_right {
+        float: right;
+        width: 200px;
+    }
+    }
 </style>
