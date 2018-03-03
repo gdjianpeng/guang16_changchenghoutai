6f73e73b041696c4ce8f70b54c1ab5bd6080039b
lijianpeng <1060397920@qq.com>
2018-03-03 11:09:30 +0800

    删除商品

--- src/component/admin/goods/list/List.vue
+++ src/component/admin/goods/list/List.vue
@@ -9,7 +9,7 @@
         <section class="list_btns">
             <el-button plain size="mini" icon="el-icon-plus">新增</el-button>
             <el-button plain size="mini" icon="el-icon-check">全选</el-button>
-            <el-button plain size="mini" icon="el-icon-delete">删除</el-button>
+            <el-button plain size="mini" icon="el-icon-delete" @click="del">删除</el-button>
 
             <div class="list_btns_right">
                 <el-input placeholder="请输入内容" prefix-icon="el-icon-search" label-width="200px" size="mini" v-model="apiQuery.searchvalue" @blur="search">
@@ -19,7 +19,7 @@
 
         <!-- 大表格 -->
         <!-- data属性用来配置表格数据  -->
-        <el-table ref="multipleTable" :data="tableData3" style="width: 100%">
+        <el-table ref="multipleTable" :data="tableData3" style="width: 100%" @selection-change="change">
 
             <!-- type为selection, 即多选框 -->
             <el-table-column type="selection" width="55"></el-table-column>
@@ -66,27 +66,17 @@ export default {
         pageSize: 10,
         searchvalue: ""
       },
+
+      // 被选中的商品数据
+      selectedGoodsList: [],
+
+       // 表格数据
       tableData3: [
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        }
+        // {
+        //   date: "2016-05-03",
+        //   name: "王小虎",
+        //   address: "上海市普陀区金沙江路 1518 弄"
+        // }
       ],
       multipleSelection: []
     };
@@ -101,7 +91,9 @@ export default {
     getGoodsData() {
       // 这个接口需要pageIndex指定页, pageSize指定每页数量, searchvalue用于商品搜索
       //   this.$http.get(this.$api.gsList + "?pageIndex=1&pageSize=10").then(res => {
-      let api = `${this.$api.gsList}?pageIndex=${this.apiQuery.pageIndex}&pageSize=${this.apiQuery.pageSize}
+      let api = `${this.$api.gsList}?pageIndex=${
+        this.apiQuery.pageIndex
+      }&pageSize=${this.apiQuery.pageSize}
       &searchvalue=${this.apiQuery.searchvalue}`;
       this.$http.get(api).then(res => {
         if (res.data.status == 0) {
@@ -109,6 +101,22 @@ export default {
         }
       });
     },
+
+     // 监听多选框状态的变化，参数可以拿到被选的商品数据
+     change(selection){
+         this.selectedGoodsList = selection;
+     },
+    // 删除按钮
+    del(){
+        let delIDs = this.selectedGoodsList.map(v=>v.id);
+        this.$http.get(this.$api.gsDel+delIDs).then((res)=>{
+            //删除成功后重新获取数据进行渲染
+            if(res.data.status ==0){
+                this.getGoodsData()
+            }
+        })
+    },
+
 
     toggleSelection(rows) {
       if (rows) {
@@ -126,6 +134,7 @@ export default {
   },
 
   // 页面一上来就自动调用接口获取表格数据进行展示
+  //当组件初始化完毕，并且数据可用时，被自动调用
   created() {
     this.getGoodsData();
   }
